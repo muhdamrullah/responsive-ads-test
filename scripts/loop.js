e0e7b4561c14ff5f6575d794bde64ba13f0bf231
muhdamrullah <muneorion@gmail.com>
2016-04-25 05:45:34 +0800

    Update loop.js

--- scripts/loop.js
+++ scripts/loop.js
@@ -1,13 +1,18 @@
 $(function(){
 
 var picCount=0;  
-var picArray= ["http://bit.ly/1uxYSSb","http://bit.ly/1oOrsO6","http://design.ubuntu.com/wp-content/uploads/ubuntu-logo32.png","http://i.giphy.com/xT1XGUd5IT0dwQLJKw.gif"];
+var picArray= [
+   'https://media.giphy.com/media/l4hLy5WvoC7mA6vPq/giphy.gif',
+   'https://media.giphy.com/media/26CYzLeA6jmwetE9W/giphy.gif',
+   'https://media.giphy.com/media/3o85fVTFUtaFUhB1Ze/giphy.gif',
+   "http://i.giphy.com/xT1XGUd5IT0dwQLJKw.gif",
+];
 nextPic();
 
 function nextPic() { 
    picCount=(picCount+1<picArray.length) ? picCount+1 : 0;
    $("#HomeRooms img").prop('src', picArray[picCount]);
    setTimeout(function() {
-       nextPic();},6000);
+       nextPic();},3000);
   }
 });
